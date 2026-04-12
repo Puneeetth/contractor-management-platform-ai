018c3cfe4404f8af91e0063cc3d1a010b6606c61
Punith <144771860+Puneeetth@users.noreply.github.com>
2026-04-12 16:24:58 +0530

    feat: setup backend spring boot

---
+++ backend/src/main/java/com/cmp/ai/CmpAiApplication.java
@@ -0,0 +1,13 @@
+package com.cmp.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmpAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmpAiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cmp/ai/CmpAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmp.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmpAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
