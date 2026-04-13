38876a49d291899b19beb38a839edcff2e4d4f6b
Punith <punithreddy417@gmail.com>
2026-04-13 17:31:02 +0530

    test: modified

--- backend/src/main/java/com/cmp/ai/repository/InvoiceRepository.java
+++ backend/src/main/java/com/cmp/ai/repository/InvoiceRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.cmp.ai.entity.Invoice;
 
 public interface InvoiceRepository extends JpaRepository<Invoice,Long>{
-    Optional<Invoice> findByTimeSheetId(Long timesheetId);
+    Optional<Invoice> findByTimesheetId(Long timesheetId);
     Optional<Invoice> findByContractorIdAndInvoiceMonth(Long contractorId,String invoiceMonth);
 }
